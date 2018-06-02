c88c3e704f5d88b7a9adece1fc6e7a5f091def50
Ken Deng <otaken120@gmail.com>
2018-06-02 10:28:54 -0400

    dataBinding Exercise
    0602

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  serverElements = [{type: 'server', name: 'Testserver', content:'Just a test!'}];
+  
+
+
+  
+}
